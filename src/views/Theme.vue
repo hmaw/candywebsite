c919da20572552241c2024860976ed7f6bdff1d3
Heather Allen <hmaw@gmail.com>
2020-05-01 16:24:00 -0700

    Added theme html contents

--- src/views/Theme.vue
+++ src/views/Theme.vue
@@ -1,6 +1,30 @@
 <template>
-	<div>
+	<div class="theme">
 		<h1>This is the Theme page</h1>
+		<h1>H1 Heading </h1>
+		<h2>h2 Heading</h2>
+		<h3>h3 Heading</h3>
+		<p>Lorem ipsum dolor sit <b>amet</b> consectetur adipisicing elit. 
+		reiciendis velit?</p>
+
+		<ol>
+			<li>Lorem ipsum dolor sitA</li>
+			<li>Lorem ipsum dolor sitB</li>
+			<li>Lorem ipsum dolor sitC</li>
+		</ol>
+
+		<ul>
+			<li>Lorem ipsum dolor sit1</li>
+			<li>Lorem ipsum dolor sit2</li>
+			<li>Lorem ipsum dolor sit3</li>
+		</ul>
+
+		<blockquote>
+		<p class="quote">	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi laudantium sequi suscipit iure voluptate fugit molestiae nam quod accusantium quasi iste, architecto fugiat numquam delectus quibusdam ipsa blanditiis nostrum excepturi? </p>
+		<p class='cite'> mag firstName lastName on date date</p>
+
+		</blockquote>
+
 	</div>
 </template>
 
