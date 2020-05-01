06c8e98a22cacce200f1406d515364c3902f2cb1
Heather Allen <hmaw@gmail.com>
2020-05-01 15:42:43 -0700

    Added Theme page

--- src/router/index.js
+++ src/router/index.js
@@ -3,25 +3,15 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
 import About from '../views/About.vue'
+import Theme from '../views/Theme.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  },
-  {
-  path: '/contact',
-  name: 'Contact',
-  component: Contact
-  }
+  {path: '/', 			name: 'Home', 		component: Home},
+  {path: '/about', 		name: 'About', 		component: About},
+  {path: '/contact', 	name: 'Contact', 	component: Contact},
+  {path: '/theme', 		name: 'Theme', 		component: Theme},
 ]
 
 const router = new VueRouter({
---
+++ src/views/Theme.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<h1>This is the Theme page</h1>
+	</div>
+</template>
+
