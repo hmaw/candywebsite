e3e811ef8f5e6797ce2d340f05bbe94b5d13b69f
Heather Allen <hmaw@gmail.com>
2020-05-04 15:00:20 -0700

    Updated Theme page contents

--- src/views/Theme.vue
+++ src/views/Theme.vue
@@ -1,9 +1,13 @@
 <template>
 	<div class="theme">
 		<h1>This is the Theme page</h1>
-		<h1>H1 Heading </h1>
-		<h2>h2 Heading</h2>
-		<h3>h3 Heading</h3>
+
+		<hgroup>
+			<h1>H1 Heading </h1>
+			<h2>h2 Heading</h2>
+			<h3>h3 Heading</h3>
+		</hgroup>
+
 		<p>Lorem ipsum dolor sit <b>amet</b> consectetur adipisicing elit. 
 		reiciendis velit?</p>
 
@@ -20,11 +24,21 @@
 		</ul>
 
 		<blockquote>
-		<p class="quote">	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi laudantium sequi suscipit iure voluptate fugit molestiae nam quod accusantium quasi iste, architecto fugiat numquam delectus quibusdam ipsa blanditiis nostrum excepturi? </p>
-		<p class='cite'> mag firstName lastName on date date</p>
-
+			<p class="quote">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi laudantium sequi suscipit iure voluptate fugit molestiae nam quod accusantium quasi iste, architecto fugiat numquam delectus quibusdam ipsa blanditiis nostrum excepturi? </p>
+			<p class='cite'>mag firstName lastName on date date</p>
 		</blockquote>
 
+		<div>
+			<button>Button 1</button>
+			<button>Button 2</button>
+			<button>Button 3</button>
+		</div>
+
+		<div>
+			<a href="http://google.com">Link to google</a>
+			<a href="http://google.com">Link to google</a>
+			<a href="http://google.com">Link to google</a>
+		 </div>
 	</div>
 </template>
 
