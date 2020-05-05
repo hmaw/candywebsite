d0ae3a4078b7c2d688e1fe1350c0830f448b0e64
Heather Allen <hmaw@gmail.com>
2020-05-05 15:57:39 -0700

    adding scaffolding to style mobile view

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,13 +1,15 @@
 <template>
 	<header class='site_header'>
-		<img alt="Vue logo image" src="../assets/logo.png">
-		<h2>Puff Daddy</h2>
 		<nav id="nav" class="site_nav">
 			<router-link to="/">Home</router-link> |
 			<router-link to="/about">About</router-link> |
 			<router-link to="/contact">Contact</router-link> |
 			<router-link to="/theme">Theme</router-link> 
 		</nav>
+		<div class='logo_container'>
+			<img alt="Vue logo image" src="../assets/logo.png">
+			<h2>Puff Daddy</h2>
+		</div>
 	</header>
 </template>
 
@@ -16,3 +18,14 @@
 		name: 'SiteHeader'
 	}
 </script>
+
+<style lang="scss">
+.site_header{
+	// box-sizing: border-box;
+}
+.logo_container{
+	padding: 10px;
+	text-align: center;
+	background-color:  lightgray;
+}
+</style>
